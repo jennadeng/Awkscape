f6e25d6e20659409d1a194b840e9b07a88b3fd50
jennadeng <jennadeng96@gmail.com>
2015-09-27 08:13:06 -0400

    Revert "pull request"

--- mobile/src/main/java/hackny/awkscape/AlarmActivity.java
+++ mobile/src/main/java/hackny/awkscape/AlarmActivity.java
@@ -13,7 +13,6 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Window;
 import android.view.WindowManager;
 
 import android.os.Handler;
@@ -27,15 +26,6 @@ public class AlarmActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        super.onCreate(savedInstanceState);
-
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-        setContentView(R.layout.activity_alarm);
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
