be6a35ccd99fc9106b401c06599a03381e006ee6
heeseungp <heeseung5@gmail.com>
2015-09-27 07:56:10 -0400

    Calling picture added

--- mobile/src/main/java/hackny/awkscape/AlarmActivity.java
+++ mobile/src/main/java/hackny/awkscape/AlarmActivity.java
@@ -13,6 +13,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 
 import android.os.Handler;
@@ -26,6 +27,15 @@ public class AlarmActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_alarm);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
